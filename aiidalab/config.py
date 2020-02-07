c7e3ac4f20502367bcb78ab34f6e57468b710a01
Simon Adorf <simon.adorf@epfl.ch>
2020-02-07 17:33:43 +0100

    Store config variables that are paths as instances of pathlib.Path.

--- aiidalab/config.py
+++ aiidalab/config.py
@@ -1,7 +1,8 @@
 """Module to manange AiiDA lab configuration."""
 from os import getenv
+from pathlib import Path
 
-AIIDALAB_HOME = getenv('AIIDALAB_HOME', '/project')
-AIIDALAB_APPS = getenv('AIIDALAB_APPS', '/project/apps')
-AIIDALAB_SCRIPTS = getenv('AIIDALAB_SCRIPTS', '/opt')
+AIIDALAB_HOME = Path(getenv('AIIDALAB_HOME', '/project'))
+AIIDALAB_APPS = Path(getenv('AIIDALAB_APPS', '/project/apps'))
+AIIDALAB_SCRIPTS = Path(getenv('AIIDALAB_SCRIPTS', '/opt'))
 AIIDALAB_REGISTRY = getenv('AIIDALAB_REGISTRY', 'https://aiidalab.materialscloud.org/appsdata/apps_meta.json')
